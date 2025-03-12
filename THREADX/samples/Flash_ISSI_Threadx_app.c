badbfd319844b7793cf58bfc7da6027fe8c406f5
Manoj A Murudi <manoj.murudi@alifsemi.com>
2025-03-12 11:48:57 +0530

    THREADX: samples: issi: remove WA for P10_7 pinmux issue from B2+ board
    Removed P10_7 alternate function work around as this is fixed from B2
    onwards rev
    
    Signed-off-by: Manoj A Murudi <manoj.murudi@alifsemi.com>

--- THREADX/samples/Flash_ISSI_Threadx_app.c
+++ THREADX/samples/Flash_ISSI_Threadx_app.c
@@ -113,7 +113,7 @@ static INT setup_pinmux(void)
     if (ret)
         return -1;
 
-    ret = pinconf_set(PORT_10, PIN_7, PINMUX_ALTERNATE_FUNCTION_1,
+    ret = pinconf_set(PORT_10, PIN_7, PINMUX_ALTERNATE_FUNCTION_7,
                      PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA | PADCTRL_READ_ENABLE);
     if (ret)
         return -1;
@@ -124,11 +124,6 @@ static INT setup_pinmux(void)
         return -1;
 
     ret = pinconf_set(PORT_8, PIN_0, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
-    if (ret)
-        return -1;
-
-    ret = pinconf_set(PORT_5, PIN_6, PINMUX_ALTERNATE_FUNCTION_1,
-                     PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
     if (ret)
         return -1;
 
